768156dd294dbc8876a7e12bb7382c2df83b6bc4
Matt Venables <matt@venabl.es>
2025-08-09 17:39:18 -0400

    Update drizzle.config.ts for production deployment

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from "drizzle-kit"
 
+const dbUrl = process.env.DATABASE_URL!
+const dbAuthToken = process.env.DATABASE_AUTH_TOKEN!
+const dialect = process.env.DATABASE_URL?.startsWith("file:")
+  ? "sqlite"
+  : "turso"
+
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/db/schema.ts",
-  dialect: "sqlite",
+  dialect,
+  verbose: true,
   dbCredentials: {
-    url: process.env.DATABASE_URL!
+    url: dbUrl,
+    authToken: dbAuthToken
   }
 })
